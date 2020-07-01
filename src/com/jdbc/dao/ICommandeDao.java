7558ce477b847426d1dacce68e827416f50055ca
Wei <zhuw17@hotmail.com>
2020-07-01 17:36:52 +0200

    commit commande

--- src/com/jdbc/dao/ICommandeDao.java
+++ src/com/jdbc/dao/ICommandeDao.java
@@ -1,7 +1,26 @@
 package com.jdbc.dao;
 
+import java.util.List;
+
 import com.librairie.model.commande.Commande;
 
-public interface ICommandeDao extends Dao<Commande> {
+public abstract class ICommandeDao implements Dao<Commande> {
+	@Override
+	public void create(Commande pObbject) {
+	}
 
+	@Override
+	public List<Commande> readAll() {
+		return null;
+	}
+
+	@Override
+	public void update(Commande pObject) {
+
+	}
+
+	@Override
+	public void delete(Commande pObject) {
+
+	}
 }
--- src/com/jdbc/dao/bdd/CommandeDaoImpl.java
+++ src/com/jdbc/dao/bdd/CommandeDaoImpl.java
@@ -1,29 +1,56 @@
 package com.jdbc.dao.bdd;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.jdbc.dao.ICommandeDao;
 import com.librairie.model.commande.Commande;
+import com.librairie.utils.Utils;
 
-public class CommandeDaoImpl implements ICommandeDao {
+public class CommandeDaoImpl extends ICommandeDao {
+	Connection connection = DatabaseConnection.getInstance().getConnection();
 
-	@Override
-	public void create(Commande pObbject) {
-		
+	public void create(String sql, Object... args) {
+		PreparedStatement preparedStatement = null;
+		try {
+			String sql = "INSERT INTO commande(numeroCmd,id_status_commande,id_client) values (?,?,?)";
+			preparedStatement = connection.prepareStatement(sql);
+			for (int i = 0; i < args.length; i++) {
+				preparedStatement.setObject(i + 1, args[i]);
+			}
+			preparedStatement.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			Utils.closeResource(connection, preparedStatement);
+		}
 	}
 
 	@Override
 	public List<Commande> readAll() {
-		return null;
+		List<Commande> commandes = new ArrayList<>();
+		try (Statement statement = connection.createStatement();
+				ResultSet result = statement
+						.executeQuery("SELECT * FROM commande INNER JOIN comprendre ON numeroCmd")) {
+			Commande tempCommande = new Commande();
+			while (result.next()) {
+				tempCommande.setRef(result.getInt("reference"));
+				tempCommande.setNombrePage(result.getInt("nombrePage"));
+				tempCommande.setPrix(result.getInt("prix"));
+				tempCommande.setQuantitee(result.getInt("quantitee"));
+				tempCommande.setTitre(result.getString("titre"));
+				commandes.add(tempCommande);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return commandes;
 	}
 
-	@Override
-	public void update(Commande pObject) {
-		
-	}
-
-	@Override
-	public void delete(Commande pObject) {
-		
-	}
 }
