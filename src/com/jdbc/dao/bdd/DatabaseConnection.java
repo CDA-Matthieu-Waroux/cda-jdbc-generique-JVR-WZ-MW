20f233e642f1f125e76ca36bc34810d49eb27bb8
Van Roosbeke <jonathan.van.roosbeke@gmail.com>
2020-07-01 11:05:13 +0200

    connection pool bdd

--- src/com/jdbc/dao/bdd/DatabaseConnection.java
+++ src/com/jdbc/dao/bdd/DatabaseConnection.java
@@ -1,5 +1,68 @@
 package com.jdbc.dao.bdd;
 
+import java.beans.PropertyVetoException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class DatabaseConnection {
 
+	// 1 mariadb - 2 mysql
+	private static int choixSgbd = 1;
+	private static DatabaseConnection databaseconnection;
+	private BasicDataSource ds;
+	private Properties properties = new Properties();
+	private FileInputStream fis;
+
+	private DatabaseConnection() throws IOException, SQLException, PropertyVetoException {
+		try {
+			fis = new FileInputStream("src\\jdbc.properties");
+			properties.load(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		ds = new BasicDataSource();
+		switch(choixSgbd) {
+		case 1 :
+			ds.setDriverClassName(properties.getProperty("MARIA_DB_DRIVER_CLASS"));
+			ds.setUrl(properties.getProperty("MARIA_DB_URL"));
+			ds.setUsername(properties.getProperty("MARIA_DB_USERNAME"));
+			ds.setPassword(properties.getProperty("MARIA_DB_PASSWORD"));
+			break;
+		case 2 :
+			ds.setDriverClassName(properties.getProperty("MYSQL_DB_DRIVER_CLASS"));
+			ds.setUrl(properties.getProperty("MYSQL_DB_URL"));
+			ds.setUsername(properties.getProperty("MYSQL_DB_USERNAME"));
+			ds.setPassword(properties.getProperty("MYSQL_DB_PASSWORD"));
+			break;
+		}
+
+		// définit le nombre minimum de connexions inactives
+		ds.setMinIdle(5);
+
+		// renvoie le nombre maximal de connexions pouvant rester inactives dans le
+		// pool.
+		ds.setMaxIdle(10);
+
+		// le nombre maximum de declaration ouvertes
+		ds.setMaxOpenPreparedStatements(100);
+
+	}
+
+	public static DatabaseConnection getInstance() throws IOException, SQLException, PropertyVetoException {
+		if (databaseconnection == null) {
+			databaseconnection = new DatabaseConnection();
+			return databaseconnection;
+		}
+		return databaseconnection;
+	}
+
+	public Connection getConnection() throws SQLException {
+		return this.ds.getConnection();
+	}
 }
--- src/com/librairie/exer/Program.java
+++ src/com/librairie/exer/Program.java
@@ -1,7 +1,20 @@
 package com.librairie.exer;
 
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.jdbc.dao.bdd.DatabaseConnection;
+
 public class Program {
 	public static void main(String[] args) {
-
+		Connection c = null;
+		try {
+			c = DatabaseConnection.getInstance().getConnection();
+			System.out.println("connecté");
+		} catch (SQLException | IOException | PropertyVetoException e) {
+			e.printStackTrace();
+		}
 	}
 }
