f4525b1751feb7d55e83bccd999a044d32c7b36b
matthieu <matthieu_waroux59@hotmail.com>
2020-07-03 17:57:37 +0200

    Correction bug du hashage et au niveau de l'entrée au clavier pour ajouter ou supprimer une demande de validation

--- src/com/librairie/utils/Hashage.java
+++ src/com/librairie/utils/Hashage.java
@@ -1,14 +1,10 @@
 package com.librairie.utils;
 
-import java.util.Random;
-
 public class Hashage {
 
 	public static String hash(String pParams) {
 
-		Random random = new Random();
-
-		pParams = BCrypt.hashpw(pParams, BCrypt.gensalt(random.nextInt(16)));
+		pParams = BCrypt.hashpw(pParams, BCrypt.gensalt(12));
 
 		return pParams;
 	}
